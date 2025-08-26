cc3a9c76b68ffcd0b6e5352a5fb8cb64acd8725c
joost <Joost>
2025-08-26 16:53:18 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "3.0.2-rc04"
+    }
+  }
+}
+
+locals {
+  talos_nodes = {
+  "talos-control-01" = {}
+  "talos-control-02" = {}
+  "talos-control-03" = {}
+  "talos-worker-01" = {}
+  "talos-worker-02" = {}
+  "talos-worker-03" = {}
+  }
+}
+
+resource "proxmox_vm_qemu" "talos" {
+ for_each    = local.talos_nodes
+ agent       = 1
+ boot        = "order=virtio0;ide2;net0"
+ memory      = 4096
+ name        = each.key
+ scsihw      = "virtio-scsi-single"
+ target_node = "pve"
+
+ disks {
+   ide {
+     ide2 {
+       cdrom {
+         iso = "DSM:iso/nocloud-amd64.iso"
+       }
+     }
+   }
+
+ virtio {
+   virtio0 {
+     disk {
+       size    = "64G"
+       storage = "local-lvm"
+     }
+   }
+  }
+ }
+  network {
+    bridge = "vmbr0"
+    id    = 0
+    model = "virtio"
+  }
+
+  cpu {
+    cores = 2
+  }
+}
